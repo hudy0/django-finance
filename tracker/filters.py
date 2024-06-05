6d3ddeafa507dccc64be0949eafd0c9230f06a92
hudy0 <hudaifa021@icloud.com>
2024-06-05 04:06:12 +0300

    Add TransactionFilter for filtering transactions

--- tracker/filters.py
+++ tracker/filters.py
@@ -1,34 +1,29 @@
+import django_filters
 from django import forms
-from django_filters import (
-    ChoiceFilter,
-    FilterSet,
-    DateFilter,
-    ModelMultipleChoiceFilter,
-)
 
 from tracker.models import Category, Transaction
 
 
-class TransactionFilter(FilterSet):
-    transaction_type = ChoiceFilter(
+class TransactionFilter(django_filters.FilterSet):
+    transaction_type = django_filters.ChoiceFilter(
         choices=Transaction.TRANSACTION_TYPE_CHOICES,
         field_name="type",
         lookup_expr="iexact",
         empty_label="Any",
     )
-    start_date = DateFilter(
+    start_date = django_filters.DateFilter(
         field_name="date",
         lookup_expr="gte",
         label="Start Date",
         widget=forms.DateInput(attrs={"type": "date"}),
     )
-    end_date = DateFilter(
+    end_date = django_filters.DateFilter(
         field_name="date",
         lookup_expr="lte",
         label="End Date",
         widget=forms.DateInput(attrs={"type": "date"}),
     )
-    category = ModelMultipleChoiceFilter(
+    category = django_filters.ModelMultipleChoiceFilter(
         queryset=Category.objects.all(),
         widget=forms.CheckboxSelectMultiple(),
         label="Category",
